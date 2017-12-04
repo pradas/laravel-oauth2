7afd8298e27021cda13a59bc7afc7f8ca2bcdc47
Gerard Pradas <pradasgsj@gmail.com>
2017-12-04 21:48:18 +0100

    added auth scaffolding

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -25,7 +26,10 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
         Passport::routes();
+
+        Passport::tokensExpireIn(Carbon::now()->addHour());
+
+        Passport::refreshTokensExpireIn(Carbon::now()->addDay());
     }
 }
